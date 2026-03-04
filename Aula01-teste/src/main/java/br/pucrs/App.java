fe45cb6acf65eb3733d409455900654b569bd9f4
Edson Moreno <51421368+empucrs@users.noreply.github.com>
2026-03-04 12:28:20 +0000

    Mudança inicial do projeto

---
+++ Aula01-teste/src/main/java/br/pucrs/App.java
@@ -0,0 +1,7 @@
+package br.pucrs;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto Aula01-teste criado com sucesso pelo Edson.");
+    }
+}
